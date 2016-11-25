a389d67841839884f8e1a14e0834f4ac917c9194
theporocenok <porocenok@bk.ru>
2016-11-25 14:49:44 +0500

    TelegramBot

---
+++ telecheckbot.py
@@ -0,0 +1,35 @@
+import telebot
+
+token = "";
+bot = telebot.TeleBot(token)
+
+chats = [297617298]
+
+def TakeTextFrom(file):
+	f = open(str(file))
+	textFROMstatustxt = f.read()
+	f.close()
+	return textFROMstatustxt
+
+@bot.message_handler(commands=['status'])
+def send_welcome(message):
+	if not message.chat.id in chats:
+		msg = bot.send_message(message.chat.id, 'Dogovorimsya?')
+		return 0
+
+	try:
+		textFROMstatus=str(TakeTextFrom("Ahtarova.txt"))
+		textFROMstatus+=str(TakeTextFrom("Ershova.txt"))
+		textFROMstatus+=str(TakeTextFrom("Gluhova.txt"))
+		textFROMstatus+=str(TakeTextFrom("Samsonova.txt"))
+		textFROMstatus+=str(TakeTextFrom("Zinnatshina.txt"))
+		msg = bot.send_message(message.chat.id, textFROMstatus)
+	except IOError:
+		msg = bot.send_message(message.chat.id, 'status.txt not found')
+
+@bot.message_handler(commands=['getid'])
+def send_welcome(message):
+	msg = bot.send_message(message.chat.id, message.chat.id)
+
+if __name__ == '__main__':
+	bot.polling(none_stop=True)
